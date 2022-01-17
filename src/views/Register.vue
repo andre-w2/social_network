a0b88972df99c5596a339ce56f5028295ae57b3c
andre-w2 <cibisova267@gmail.com>
2022-01-18 01:28:00 +0500

    improved registration

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -7,28 +7,29 @@
                         <div class="text-center mb-3">
                             <img src="https://pngimg.com/uploads/instagram/instagram_PNG5.png" class="text-secondary mb-2 size" />
                             <p class="text-muted fw-bold">
-                                Зарегистрируйтесь, чтобы смотреть фото и видео ваших друзей.
+                                Зарегистрируйтесь, чтобы смотреть фото ваших друзей.
                             </p>
                         </div>
                         <div class="form-floating mb-3">
-                            <input class="form-control" placeholder="Mobile Number or Email" type="email" /><label>эл. адрес</label>
+                            <input class="form-control" placeholder="Email" type="email" v-model="email" />
+                            <label>эл. адрес</label>
                         </div>
                         <div class="form-floating mb-3">
-                            <input class="form-control" placeholder="Full Name" type="text" /><label>Имя и фамилия</label>
+                            <input class="form-control" placeholder="Name" type="text" v-model="name" />
+                            <label>Имя и фамилия</label>
                         </div>
                         <div class="form-floating mb-3">
-                            <input class="form-control" placeholder="Username" type="text" /><label>Имя пользователя</label>
+                            <input class="form-control" placeholder="Usename" type="text" v-model="username" />
+                            <label>Имя пользователя</label>
                         </div>
                         <div class="form-floating mb-3">
-                            <input class="form-control" placeholder="Password" type="password" /><label>Пароль</label>
+                            <input class="form-control" placeholder="password" type="password" v-model="pass" />
+                            <label>Пароль</label>
                         </div>
                         <div class="mb-2">
                             <button 
                                 class="btn btn-primary fw-bold w-100 bg-gradient" 
                                 :disabled="isSubmutting">Далее</button>
-                        </div>
-                        <div class="small text-center">
-                            Регистрируясь, вы принимаете наши Условия. Прочитайте Политику использования данных, чтобы узнать, как мы получаем, используем и передаем ваши данные, а также просмотрите Политику в отношении файлов cookie, чтобы узнать, как мы используем файлы cookie и подобные технологии.
                         </div>
                     </form>
                     <div class="bg-white py-4 px-5 text-center border mt-4">
@@ -44,6 +45,14 @@
 <script>
 export default {
     name: 'MvcRegister',
+    data() {
+        return {
+            email: '',
+            name: '',
+            username: '',
+            pass: ''
+        }
+    },
     computed: {
         isSubmutting() {
             return this.$store.state.auth.isSubmutting
@@ -52,10 +61,10 @@ export default {
     methods: {
         onSubmit() {
             this.$store.dispatch('register', {
-                email: 'authJS@gmail.com',
-                name: 'Alex21',
-                username: 'Retro_smusic',
-                password: 'sdfsd12fs'
+                email: this.email,
+                name: this.name,
+                username: this.username,
+                password: this.pass
             }).then(user => {
                 console.log(user)
             })
