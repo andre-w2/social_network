7a172630c65981cfb0b7e3f9537ea43587ba63fe
Andrew <chibisov.a@internet.ru>
2022-02-21 18:04:08 +0500

    добавлен TopBar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <template>
-	test
+	<mvc-top-bar />
 	<router-view></router-view>
 </template>
+
+<script>
+import MvcTopBar from '@/components/TopBar';
+
+export default {
+	name: 'MvcApp',
+	components: {
+		MvcTopBar
+	}
+}
+</script>
